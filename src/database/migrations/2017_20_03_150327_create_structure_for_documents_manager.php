a7e0e25d94f7f98ac3c3d42e426285cb44b3b9ed
mishu <mishu@xtelecom.ro>
2017-03-25 13:13:29 +0200

    fix protected properties

--- src/database/migrations/2017_20_03_150327_create_structure_for_documents_manager.php
+++ src/database/migrations/2017_20_03_150327_create_structure_for_documents_manager.php
@@ -4,11 +4,11 @@ use LaravelEnso\Core\App\Classes\StructureManager\StructureMigration;
 
 class CreateStructureForDocumentsManager extends StructureMigration
 {
-    private $permissionsGroup = [
+    protected $permissionsGroup = [
         'name' => 'system.documents', 'description' => 'Documents Permissions Group',
     ];
 
-    private $permissions = [
+    protected $permissions = [
         ['name' => 'core.documents.upload', 'description' => 'Upload Docs', 'type' => 1],
         ['name' => 'core.documents.list', 'description' => 'List Documents for Documentable', 'type' => 0],
         ['name' => 'core.documents.show', 'description' => 'Show Document', 'type' => 0],
