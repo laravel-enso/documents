0a53ffcf7e3df7c2582558992ea70d39369a88ff
aocneanu <aocneanu@gmail.com>
2017-06-25 12:00:51 +0000

    Apply fixes from StyleCI

--- src/app/Traits/Documentable.php
+++ src/app/Traits/Documentable.php
@@ -4,8 +4,8 @@ namespace LaravelEnso\DocumentsManager\app\Traits;
 
 trait Documentable
 {
-	public function documents()
-	{
-	    return $this->morphMany('LaravelEnso\DocumentsManager\app\Models\Document', 'documentable');
-	}
+    public function documents()
+    {
+        return $this->morphMany('LaravelEnso\DocumentsManager\app\Models\Document', 'documentable');
+    }
 }
--- src/app/Traits/Documents.php
+++ src/app/Traits/Documents.php
@@ -4,7 +4,7 @@ namespace LaravelEnso\DocumentsManager\app\Traits;
 
 trait Documents
 {
-	public function documents()
+    public function documents()
     {
         return $this->hasMany('LaravelEnso\DocumentsManager\app\Models\Document', 'created_by');
     }
