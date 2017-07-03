3815a76d2fc1b2f09f050941819bf16d17130ddb
aocneanu <aocneanu@gmail.com>
2017-07-03 11:16:55 +0000

    Apply fixes from StyleCI

--- src/config/documents.php
+++ src/config/documents.php
@@ -2,8 +2,8 @@
 
 return [
     'deletableTimeLimitInHours' => 1,
-    'imageWidth' => 2048,
-    'imageHeight' => 2048,
+    'imageWidth'                => 2048,
+    'imageHeight'               => 2048,
     'documentables'             => [
         'owner' => 'App\Owner',
     ],
