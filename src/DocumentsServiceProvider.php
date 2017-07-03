10cf9b22c71668f1b53639a5fa5be199f5db4852
aocneanu <aocneanu@gmail.com>
2017-07-03 11:34:05 +0000

    Apply fixes from StyleCI

--- src/DocumentsServiceProvider.php
+++ src/DocumentsServiceProvider.php
@@ -15,27 +15,27 @@ class DocumentsServiceProvider extends ServiceProvider
     private function publishesAll()
     {
         $this->publishes([
-            __DIR__ . '/config/documents.php' => config_path('documents.php'),
+            __DIR__.'/config/documents.php' => config_path('documents.php'),
         ], 'documents-config');
 
         $this->publishes([
-            __DIR__ . '/resources/assets/js/components' => resource_path('assets/js/vendor/laravel-enso/components'),
+            __DIR__.'/resources/assets/js/components' => resource_path('assets/js/vendor/laravel-enso/components'),
         ], 'documents-component');
 
         $this->publishes([
-            __DIR__ . '/config' => config_path(),
+            __DIR__.'/config' => config_path(),
         ], 'enso-config');
 
         $this->publishes([
-            __DIR__ . '/resources/assets/js/components' => resource_path('assets/js/vendor/laravel-enso/components'),
+            __DIR__.'/resources/assets/js/components' => resource_path('assets/js/vendor/laravel-enso/components'),
         ], 'enso-update');
     }
 
     private function loadDependencies()
     {
-        $this->mergeConfigFrom(__DIR__ . '/config/documents.php', 'documents');
-        $this->loadRoutesFrom(__DIR__ . '/routes/web.php');
-        $this->loadMigrationsFrom(__DIR__ . '/database/migrations');
+        $this->mergeConfigFrom(__DIR__.'/config/documents.php', 'documents');
+        $this->loadRoutesFrom(__DIR__.'/routes/web.php');
+        $this->loadMigrationsFrom(__DIR__.'/database/migrations');
     }
 
     public function register()
