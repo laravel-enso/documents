14f3652d928ae8e478592c0a960ef573f228ec96
mishu <mishu@xtelecom.ro>
2017-07-12 15:38:36 +0300

    updated composer.json for package auto discovery. refactor routes

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,14 +1,19 @@
 <?php
 
-Route::group([
-    'namespace'  => 'LaravelEnso\DocumentsManager\app\Http\Controllers',
-    'middleware' => ['web', 'auth', 'core'],
-], function () {
-    Route::group(['prefix' => 'core/documents', 'as' => 'core.documents.'], function () {
-        Route::post('upload/{type}/{id}', 'DocumentController@upload')->name('upload');
-        Route::get('show/{document}', 'DocumentController@show')->name('show');
-        Route::get('download/{document}', 'DocumentController@download')->name('download');
-        Route::delete('destroy/{document}', 'DocumentController@destroy')->name('destroy');
-        Route::get('/{type}/{id}', 'DocumentController@index')->name('index');
+Route::middleware(['web', 'auth', 'core'])
+    ->namespace('LaravelEnso\DocumentsManager\app\Http\Controllers')
+    ->group(function () {
+        Route::prefix('core/documents')->as('core.documents.')
+            ->group(function () {
+                Route::post('upload/{type}/{id}', 'DocumentController@upload')
+                    ->name('upload');
+                Route::get('show/{document}', 'DocumentController@show')
+                    ->name('show');
+                Route::get('download/{document}', 'DocumentController@download')
+                    ->name('download');
+                Route::delete('destroy/{document}', 'DocumentController@destroy')
+                    ->name('destroy');
+                Route::get('/{type}/{id}', 'DocumentController@index')
+                    ->name('index');
+            });
     });
-});
