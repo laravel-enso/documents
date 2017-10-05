3e3d056c21506fcd6e8ef75d815ca1453b80c076
mishu <mishu@xtelecom.ro>
2017-10-05 21:26:18 +0300

    fix routes

--- src/routes/api.php
+++ src/routes/api.php
@@ -1,20 +1,17 @@
 <?php
 
-Route::middleware('api')
-    ->prefix('api')
+Route::middleware(['web', 'auth', 'core'])
+    ->prefix('api/core/documents')->as('core.documents.')
     ->namespace('LaravelEnso\DocumentsManager\app\Http\Controllers')
     ->group(function () {
-        Route::prefix('core/documents')->as('core.documents.')
-            ->group(function () {
-                Route::post('upload/{type}/{id}', 'DocumentController@store')
-                    ->name('upload');
-                Route::get('show/{document}', 'DocumentController@show')
-                    ->name('show');
-                Route::get('download/{document}', 'DocumentController@download')
-                    ->name('download');
-                Route::delete('destroy/{document}', 'DocumentController@destroy')
-                    ->name('destroy');
-                Route::get('/{type}/{id}', 'DocumentController@index')
-                    ->name('index');
-            });
+        Route::post('upload/{type}/{id}', 'DocumentController@store')
+            ->name('upload');
+        Route::get('show/{document}', 'DocumentController@show')
+            ->name('show');
+        Route::get('download/{document}', 'DocumentController@download')
+            ->name('download');
+        Route::delete('destroy/{document}', 'DocumentController@destroy')
+            ->name('destroy');
+        Route::get('/{type}/{id}', 'DocumentController@index')
+            ->name('index');
     });
