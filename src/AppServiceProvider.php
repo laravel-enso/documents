0ee06dd339581ffa68d2fe710a99c131a85b1651
mishu <mishu@xtelecom.ro>
2018-01-11 10:53:15 +0200

    renames app service provider

--- src/AppServiceProvider.php
+++ src/AppServiceProvider.php
@@ -4,7 +4,7 @@ namespace LaravelEnso\DocumentsManager;
 
 use Illuminate\Support\ServiceProvider;
 
-class DocumentsServiceProvider extends ServiceProvider
+class AppServiceProvider extends ServiceProvider
 {
     public function boot()
     {
