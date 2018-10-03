0cbdcf86742ee75408eb7edc37dc28a70c4fc95b
mishu <mishu@xtelecom.ro>
2018-10-03 09:24:56 +0300

    makes created_by nullable

--- src/database/migrations/2017_01_01_140000_create_documents_table.php
+++ src/database/migrations/2017_01_01_140000_create_documents_table.php
@@ -13,9 +13,8 @@ class CreateDocumentsTable extends Migration
 
             $table->morphs('documentable');
 
-            $table->integer('created_by')->unsigned();
-            $table->foreign('created_by')->references('id')
-                ->on('users');
+            $table->integer('created_by')->unsigned()->nullable();
+            $table->foreign('created_by')->references('id')->on('users');
 
             $table->timestamps();
         });
