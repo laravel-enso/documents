9c163db8c1944c2af7ad5ecab552ad77893f8bf0
mishu <mishu@xtelecom.ro>
2018-10-11 08:28:24 +0300

    adds `onDelete` config option / improves `Documentable` trait

--- src/app/Traits/Documentable.php
+++ src/app/Traits/Documentable.php
@@ -3,9 +3,28 @@
 namespace LaravelEnso\DocumentsManager\app\Traits;
 
 use LaravelEnso\DocumentsManager\app\Models\Document;
+use Symfony\Component\HttpKernel\Exception\ConflictHttpException;
 
 trait Documentable
 {
+    public static function bootDocumentable()
+    {
+        self::deleting(function ($model) {
+            if (config('enso.documents.onDelete') === 'restrict'
+                && $model->documents()->first() !== null) {
+                throw new ConflictHttpException(
+                    __('The entity has attached documents and cannot be deleted')
+                );
+            }
+        });
+
+        self::deleted(function ($model) {
+            if (config('enso.documents.onDelete') === 'cascade') {
+                $model->documents()->delete();
+            }
+        });
+    }
+
     public function documents()
     {
         return $this->morphMany(Document::class, 'documentable');
