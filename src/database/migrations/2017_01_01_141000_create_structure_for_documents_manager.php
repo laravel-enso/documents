2f6a5ffdfcbb72c2b415fb7e43def694865a3877
mishu <mishu@xtelecom.ro>
2018-10-25 09:39:01 +0300

    updates structure migration

--- src/database/migrations/2017_01_01_141000_create_structure_for_documents_manager.php
+++ src/database/migrations/2017_01_01_141000_create_structure_for_documents_manager.php
@@ -4,10 +4,6 @@ use LaravelEnso\StructureManager\app\Classes\StructureMigration;
 
 class CreateStructureForDocumentsManager extends StructureMigration
 {
-    protected $permissionGroup = [
-        'name' => 'core.documents', 'description' => 'Documents permissions group',
-    ];
-
     protected $permissions = [
         ['name' => 'core.documents.store', 'description' => 'Upload documents', 'type' => 1, 'is_default' => false],
         ['name' => 'core.documents.index', 'description' => 'List documents for documentable', 'type' => 0, 'is_default' => false],
