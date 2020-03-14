e311ec1c28948dd153f00d24608cecc6c8e9f3b2
mishu <mishu@xtelecom.ro>
2020-03-14 18:26:50 +0200

    removes Types from migration

--- src/database/migrations/2017_01_01_141000_create_structure_for_documents.php
+++ src/database/migrations/2017_01_01_141000_create_structure_for_documents.php
@@ -1,13 +1,12 @@
 <?php
 
 use LaravelEnso\Migrator\App\Database\Migration;
-use LaravelEnso\Permissions\App\Enums\Types;
 
 class CreateStructureForDocuments extends Migration
 {
     protected $permissions = [
-        ['name' => 'core.documents.store', 'description' => 'Upload documents', 'type' => Types::Write, 'is_default' => false],
-        ['name' => 'core.documents.index', 'description' => 'List documents for documentable', 'type' => Types::Read, 'is_default' => false],
-        ['name' => 'core.documents.destroy', 'description' => 'Delete document', 'type' => Types::Write, 'is_default' => false],
+        ['name' => 'core.documents.store', 'description' => 'Upload documents', 'is_default' => false],
+        ['name' => 'core.documents.index', 'description' => 'List documents for documentable', 'is_default' => false],
+        ['name' => 'core.documents.destroy', 'description' => 'Delete document', 'is_default' => false],
     ];
 }
