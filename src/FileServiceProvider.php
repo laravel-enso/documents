ce2e1af249e87ba1f8d64b3eda860128a6138ee1
Mihai <mihai.ocneanu@gmail.com>
2020-05-15 12:36:59 +0000

    Apply fixes from StyleCI

--- src/FileServiceProvider.php
+++ src/FileServiceProvider.php
@@ -2,7 +2,6 @@
 
 namespace LaravelEnso\Documents;
 
-use LaravelEnso\Documents\App\Models\Document;
 use LaravelEnso\Files\FileServiceProvider as ServiceProvider;
 
 class FileServiceProvider extends ServiceProvider
