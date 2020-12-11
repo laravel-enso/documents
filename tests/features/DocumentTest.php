8390881246d908cb16e735000c6f7648fea6ae07
Mihai <mihai.ocneanu@gmail.com>
2020-12-11 13:54:14 +0200

    reverted to using assertExists

--- tests/features/DocumentTest.php
+++ tests/features/DocumentTest.php
@@ -55,7 +55,7 @@ class DocumentTest extends TestCase
 
         $document = $this->testModel->documents()->first();
 
-        $this->assertTrue(Storage::exists($document->file->path));
+        Storage::assertExists($document->file->saved_name);
     }
 
     /** @test */
