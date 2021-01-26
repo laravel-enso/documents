440d8500012d7acde14c3b9d20e1e8d74e4b413f
reza <raftx24@gmail.com>
2021-01-26 15:51:48 +0330

    adds observer & register

---
+++ src/Observers/Observer.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace LaravelEnso\Documents\Observers;
+
+use LaravelEnso\Discussions\Exceptions\DiscussionConflict;
+use LaravelEnso\Documents\Exceptions\DocumentConflict;
+
+class Observer
+{
+    private function deleting()
+    {
+        if (config('enso.documents.onDelete') === 'restrict'
+            && $this->documents()->exists()) {
+            throw DocumentConflict::delete();
+        }
+    }
+
+    private function deleted()
+    {
+        if (config('enso.documents.onDelete') === 'cascade') {
+            $this->documents()->delete();
+        }
+    }
+}
---
+++ src/Services/Register.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace LaravelEnso\Documents\Services;
+
+use LaravelEnso\Documents\Observers\Observer;
+use LaravelEnso\Documents\DynamicRelations\Documents;
+use LaravelEnso\DynamicMethods\Services\Methods;
+
+class Register
+{
+    public static function handle(string $model)
+    {
+        Methods::bind($model, [Documents::class]);
+        $model::observe(Observer::class);
+    }
+}
