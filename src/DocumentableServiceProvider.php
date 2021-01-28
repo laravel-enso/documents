c05eaa3c0c81febf90e209b04e1e9ac80f93b587
reza <raftx24@gmail.com>
2021-01-28 14:39:41 +0330

    use ServiceProvider for register relation instead of Register class

---
+++ src/DocumentableServiceProvider.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace LaravelEnso\Documents;
+
+use Illuminate\Support\Collection;
+use Illuminate\Support\ServiceProvider;
+use LaravelEnso\Documents\Observers\Observer;
+use LaravelEnso\Documents\DynamicRelations\Documents;
+use LaravelEnso\DynamicMethods\Services\Methods;
+
+class DocumentableServiceProvider extends ServiceProvider
+{
+    protected array $register = [];
+
+    public function boot()
+    {
+        Collection::wrap($this->register)
+            ->each(function ($model) {
+                Methods::bind($model, [Documents::class]);
+                $model::observe(Observer::class);
+            });
+    }
+}
